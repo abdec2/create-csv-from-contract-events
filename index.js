39218c540d0723264ceaa28d9feee0cf3820efd6
Azim Baig <abdec2@hotmail.com>
2024-10-29 17:34:38 +0300

    claim list generated

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const { format } = require('fast-csv');
 
 const publicClient = createPublicClient({
     chain: mainnet,
-    transport: http()
+    transport: http('https://eth-mainnet.g.alchemy.com/v2/9WJbt3XOcSzhJf-XXudD4KyW9DSNdomO')
 })
 
 const contractAddress = '0xe54a7e5615Ac3236E415Fd9Fead420adeACb92eA';
 
-const getLogs = async () => {
+const getLogs = async (blockfrom, blockto) => {
     const logs = await publicClient.getContractEvents({ 
         address: contractAddress,
         abi: abi,
         eventName: 'Claimed',
         fromBlock: 20952799n,
+        toBlock: 21070119n,
     })
     
     if(logs.length > 0) {
@@ -32,7 +33,7 @@ const getLogs = async () => {
         // Pipe the CSV stream to the writable stream
         csvStream.pipe(ws);
         logs.map(log => {
-            csvStream.write({user: log.args.user, amount: formatUnits(log.args.amount, 18), timeStamp: log.args.timestamp});
+            csvStream.write({user: log.args.user, amount: formatUnits(log.args.amount, 18), blockNumber: log.blockNumber, timeStamp: new Date(Number(log.args.timestamp.toString()) * 1000).toISOString() });
         })
 
         csvStream.end();
@@ -40,4 +41,9 @@ const getLogs = async () => {
     }
 }
 
-getLogs()
+let blockStart = BigInt(20952799)
+
+do {
+    getLogs(blockStart, blockStart+BigInt(800))
+    blockStart += BigInt(800) 
+} while (blockStart >= BigInt(21070119))
